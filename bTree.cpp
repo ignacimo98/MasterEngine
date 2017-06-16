f3d0ff25a720df817c9a6a4aa921c88a0c317f22
Ignacio Mora <ignacimo98@hotmail.com>
2017-06-16 17:45:53 -0600

    Funciona con otro programa y no solo con telnet

---
+++ bTree.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Ignacio Mora on 6/15/17.
+//
+
+#include "bTree.h"
---
+++ bTree.h
@@ -0,0 +1,14 @@
+//
+// Created by Ignacio Mora on 6/15/17.
+//
+
+#ifndef MASTERENGINE_BTREE_H
+#define MASTERENGINE_BTREE_H
+
+
+class bTree {
+
+};
+
+
+#endif //MASTERENGINE_BTREE_H
