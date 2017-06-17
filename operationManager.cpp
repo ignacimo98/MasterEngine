6c214b5d994c6f5ba514ef53d8076f6e63772ac7
jimena <jimenamaria598@gmail.com>
2017-06-16 19:23:04 -0600

    select

--- operationManager.cpp
+++ operationManager.cpp
@@ -2,7 +2,42 @@
 // Created by jimena on 16/06/17.
 //
 #include "operationManager.h"
+#include "where.h"
+#include "join.h"
 
-Table OperationManager::select(json json) {
+Table OperationManager::select(json json){
+    if(json["where"] == "" && json["join"] == "")
+        selectAux(json["from"], json["what"]);
+
+    else if(json["where"] == "" && json["join"] != "") {
+        Join joinObject(json["join"]);
+        selectAux(json["from"], json["what"], joinObject);
+    }
+    else if(json["where"] != "" && json["join"] == ""){
+        Where whereObject(json["where"]);
+        selectAux(json["from"], json["what"], whereObject);
+    }
+    else{
+        Where whereObject(json["where"]);
+        Join joinObject(json["join"]);
+        selectAux(json["from"], json["what"], whereObject, joinObject);
+    }
+}
+
+
+Table selectAux(std::string tableName, std::vector<std::string> columns, Where whereObject, Join joinObject){
     
 }
+
+Table selectAux(std::string tableName, std::vector<std::string> columns, Where whereObject){
+
+}
+
+Table selectAux(std::string tableName, std::vector<std::string> columns, Join joinObject){
+
+}
+
+Table selectAux(std::string tableName, std::vector<std::string> columns){
+
+}
+
